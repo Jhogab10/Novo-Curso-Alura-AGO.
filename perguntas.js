407bd403d734b117d2c3fbc34229302ab944ad26
Gabriel Bg <105066266+Jhogab10@users.noreply.github.com>
2024-08-12 11:35:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,86 @@
+export const perguntas = [
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ["",
+                    ""
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: ["",
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ["",
+                    "",
+                    ""
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: ["",
+                    "",
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ["",
+                    ""
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: ["",
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ["",
+                    ""
+                ]
+            },
+            {
+                texto: "",
+                afirmacao: ["",
+                    "",
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ""
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
