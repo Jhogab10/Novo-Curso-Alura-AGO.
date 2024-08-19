7b0479bc10ba80caba56b21947f5f0b0ce726b49
Gabriel Bg <105066266+Jhogab10@users.noreply.github.com>
2024-08-19 11:38:04 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,17 @@
+export const perguntas = [
+    {
+        enunciado: "Qual a sua cor favorita?",
+        alternativas: [
+            { texto: "Azul", afirmacao: "Você gosta de azul.", proxima: 1 },
+            { texto: "Verde", afirmacao: "Você gosta de verde.", proxima: 2 }
+        ]
+    },
+    {
+        enunciado: "Qual o seu animal favorito?",
+        alternativas: [
+            { texto: "Cachorro", afirmacao: "Você gosta de cachorro.", proxima: 3 },
+            { texto: "Gato", afirmacao: "Você gosta de gato.", proxima: 4 }
+        ]
+    },
+    // Adicione mais perguntas conforme necessário
+];
